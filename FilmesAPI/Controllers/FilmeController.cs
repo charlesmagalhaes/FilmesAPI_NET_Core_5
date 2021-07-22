01a0636f857df80f35823a4a88bd507bb50984ac
Charles Magalhães <charlesrm29@gmail.com>
2021-07-21 22:20:28 -0300

    Criando Arquivo FilmeController.cs e Filme.cs. Criando endponts get e post.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,33 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Duracao);
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RetornarFilmes()
+        {
+            return filmes;
+        }
+
+    }
+}
