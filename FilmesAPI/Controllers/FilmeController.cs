a89c6b3e80c4db8342aa4a57963a8242810decb2
Charles Magalhães <charlesrm29@gmail.com>
2021-07-26 00:41:10 -0300

    Criando os metodos delete e put.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -16,9 +16,9 @@ namespace FilmesAPI.Controllers
 
         public FilmeController(FilmeContext context)
         {
-            _context = context;    
+            _context = context;
         }
-      
+
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
@@ -39,14 +39,43 @@ namespace FilmesAPI.Controllers
         public IActionResult RecuperaFilmesPorId(int id)
         {
             Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
-            if(filme != null)
+            if (filme != null)
             {
                 return Ok(filme);
             }
             return NotFound();
-           
+
         }
-       
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id,[FromBody] Filme filmeNovo)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            filme.Titulo = filmeNovo.Titulo;
+            filme.Genero = filmeNovo.Genero;
+            filme.Duracao = filmeNovo.Duracao;
+            filme.Diretor = filmeNovo.Diretor;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilmesPorID(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
 
     }
 }
